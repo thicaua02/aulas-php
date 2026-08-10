e6deb1a4116d97cbc33434d4d91bca1f2bd86556
Thiago <thiagocaua049@gmail.com>
2026-08-10 08:37:20 -0300

    Removendo a formatação e aplicando o bootstrap.

--- 01-sintaxe-geral.php
+++ 01-sintaxe-geral.php
@@ -5,15 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Sintaxe Geral</title>
-    <style>
-        .exemplo {
-            border: solid 4px;
-            padding: 4px;
-            /*Propriedade CSS cujo valor
-            vem de um processamento PHP*/
-            color: <?php echo "orange" ?>;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
@@ -30,6 +22,7 @@
         echo "<p><abbr title='Hyper Preprocessor'>PHP</abbr> é uma linguagem popular.</p>";
     ?>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script>
         const titulo = document.getElementsByTagName("h1")[0];
         titulo.addEventListener('click', () => {
