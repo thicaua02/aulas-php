39705ca7132739668fa6ce1da258a70bd43e3de4
Thiago <thiagocaua049@gmail.com>
2026-08-19 09:16:17 -0300

    Salvando.

---
+++ 15-filtros.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Filtros para validação e sanitização</h1>
+        <hr>
+        <p>Filtros são recursos de análise e limpeza de dados aplicados através de uma 
+        função especiais, normalmente as funções <code>filter_var</code> e <code>filter_input</code>
+        e também om o uso de <b>constantes</b> de validação/senitização.</p>
+
+        <h2>Validação</h2>
+        <h3>FILTER_VALIDATE_EMAIL</h3>
+        <?php
+            $email = "email@provedor.com";
+            $emailValido = filter_var($email, FILTER_VALIDATE_EMAIL); 
+        ?>
+
+        <pre><?php var_dump($emailValido) ?></pre>
+        <?php if($emailValido): ?>
+            <p class="text-success">E-mail correto!</p>
+        <?php else: ?>
+            <p class="text-danger">E-mail incorreto!</p>
+        <?php endif ?>
+
+        <h3>FILTER_VALIDATE_URL</h3>
+        <?php
+            $redeSocial = "https://linkedin.com/in/tiagobsantos";
+            $redeSocialValida = filter_var($redeSocial, FILTER_VALIDATE_URL); 
+        ?>
+            <pre><?php var_dump($redeSocialValida); ?></pre>
+        <?php if($redeSocialValida): ?>
+            <a href="<?= $redeSocial ?>" class="btn btn-info">Me siga no LinkedIn</a>
+        <?php endif ?>
+
+        <hr>
+
+        <h2>Sanitização</h2>
+        <h3>FILTER_SANITIZE_EMAIL</h3>
+
+        <?php
+            $contato = "/thiago@gmail.com";
+            $contatoSanitizado = filter_var($contato, FILTER_SANITIZE_EMAIL); 
+        ?>
+
+        <p>Contato <b>sem</b> sanitização: <?= $contato ?></p>
+        <p>Contato <b>com</b> sanitização: <?= $contatoSanitizado ?></p>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
