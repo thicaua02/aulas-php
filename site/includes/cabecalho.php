10d8463dabf2cee4114b3b3d981ee3e30984ae0f
Thiago <thiagocaua049@gmail.com>
2026-08-19 11:53:31 -0300

    Milene Eufrasio.

---
+++ site/includes/cabecalho.php
@@ -0,0 +1,21 @@
+<?php 
+    const BASE = "/site/";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site usando PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Site com PHP</h1>
+        <nav>
+            <a href="<?= BASE ?>index.php">Home</a>
+            <a href="<?= BASE ?>cursos.php">Cursos</a>
+            <a href="<?= BASE ?>duvidas.php">Dúvidas</a>
+        </nav>
+    </header>
+
+    <main>
---
+++ site/index.php
@@ -0,0 +1,6 @@
+<?php require "includes/cabecalho.php" ?>
+
+<h2>Bem-vindo ao site de exemplo</h2>
+<p>Está é a primeira página do nosso site</p>
+
+<?php require "includes/rodape.php" ?>
